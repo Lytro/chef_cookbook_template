db168f10f709cf49d078e671865a88fa36893413
Anuj Biyani <abiyani@lytro.com>
2013-01-14 20:38:31 -0800

    add librarian-chef config option, update spec_helper with options for running dependent cookbooks or stubbing them out

--- README.md
+++ README.md
@@ -15,4 +15,12 @@ Usage
 2. Search and replace "chef_cookbook_template" with the name of your cookbook (preserve case if you want to be fancy)
 3. Update the maintainer and maintainer_email in `metadata.rb`
 4. Update YOUR_COMPANY_NAME towards the top of `recipes/default.rb`
-4. Start TDD-ing your cookbook!
+5. Start TDD-ing your cookbook!
+
+* If you want your specs to run against dependent cookbooks:
+    1. add them to the `Cheffile`
+    2. install them via `librarian-chef install`
+    3. open `spec/spec_helper.rb`, then delete line 10 and uncomment lines 11 and 12.
+* If you want to stub out dependent cookbooks:
+    1. open `spec/spec_helper.rb` and uncomment lines 17, 18 and 19.
+* If you don't have dependent cookbooks, then you need not do a thing!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,18 @@ Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
 
 RSpec.configure do |config|
   config.before(:each) do
+    # Delete this next line and uncomment the two below it if you are using Librarian-Chef to download cookbooks and
+    # want ChefSpec to test against stuff inside of those cookbooks
+
     @runner = ChefSpec::ChefRunner.new
+    #cookbook_paths = %W(#{File.expand_path(Dir.pwd)}/cookbooks #{File.expand_path("..", Dir.pwd)})
+    #@runner = ChefSpec::ChefRunner.new({cookbook_path: cookbook_paths})
+
+
+    # Uncomment these lines if you want to stub the loading of cookbooks instead of actually sourcing them.
+
+    #Chef::Recipe.any_instance.stub(:load_recipe).and_return do |arg|
+    #  runner.node.run_state[:seen_recipes][arg] = true
+    #end
   end
 end
